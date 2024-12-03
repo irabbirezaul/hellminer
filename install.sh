e32c2066939b6a3406562ec342e9cd18f8af192d
Rezaull Islam <99371232+irabbirezaul@users.noreply.github.com>
2024-12-03 07:04:15 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ sudo apt-get install screen -y
 wget https://github.com/irabbirezaul/hellminer/archive/refs/heads/main.zip
 unzip main.zip
 cd hellminer-main
+sudo apt install libsodium-dev -y && wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xvzf hellminer_linux64.tar.gz 
 sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
-echo "@reboot sleep 60 && /$USER/hellminer/dotasks.sh" >> mycron
+echo "@reboot sleep 60 && /$USER/hellminer-main/dotasks.sh" >> mycron
 crontab mycron
 rm mycron
 sudo systemctl enable cron.service
 update-rc.d cron defaults
 sudo chmod +x hellminer
 sudo chmod +x mine.sh
-sudo chmod +x verus-solver
-screen -d -m bash -c "cd hellminer ; ./mine.sh" &
+screen -d -m bash -c "cd hellminer-main ; ./mine.sh" &
