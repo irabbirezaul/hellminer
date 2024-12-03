f364aca3bc81349df293e8fc57a24e62aa581ebc
Rezaull Islam <99371232+irabbirezaul@users.noreply.github.com>
2024-12-03 06:50:48 +0600

    Delete auto_screen.sh

--- auto_screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sessions=$(screen -ls | grep -oP '^\s*\d+(?=\.)')
-if [ "$(echo "$sessions" | wc -l)" -eq 1 ]; then
-    screen -r "$sessions"
-else
-    echo "Multiple or no screen sessions found."
-    screen -ls
-fi
