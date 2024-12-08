b5a0859dfbf483d19db277b5ab8226c90f082a72
Rezaull Islam <99371232+irabbirezaul@users.noreply.github.com>
2024-12-08 13:03:49 +0600

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RTXJJtDXZxu9f6ritudxTMjf4u2Sdxq56g.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RTXJJtDXZxu9f6ritudxTMjf4u2Sdxq56g.$RANDOM -p x --cpu "$(nproc)"
